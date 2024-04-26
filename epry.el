c69cfff7f59d062cf98234f8f3312254681b3f9b
Quint Pieters <quintgorssel@gmail.com>
2024-04-26 14:59:20 +0200

    feat: Re-enabled linting and restructered

--- epry.el
+++ epry.el
@@ -25,15 +25,163 @@
 (let ((src-dir (expand-file-name "src" epry-dir)))
   (add-to-list 'load-path src-dir))
 
-(defun epry-load-files ()
-  "Load all the necessary files for the Epry package."
-  (interactive)
-  (require 'epry-ui)
-  (require 'epry-mode)
-  (require 'epry-core))
+(defgroup epry nil
+  "Customization group for EPry."
+  :prefix "epry-"
+  :group 'applications)
 
-;; Call the function to load all files when Epry is started
-;(epry-load-files)
+(defcustom epry-shell-path "/opt/homebrew/bin/fish" ; "/bin/bash"
+  "Path to the shell used by EPry for executing commands."
+  :type 'string
+  :group 'epry)
+
+(defcustom epry-prefix-command "mise x -- " ; ""
+  "Prefix command because we run in a non-interactive shell."
+  :type 'string
+  :group 'epry)
+
+(defun epry-project-root ()
+  "Return the root directory of the project."
+  (or
+   ;; Try to find the Git root via VC first
+   (vc-root-dir)
+   ;; Fallback to projectile if vc-root-dir is not available or fails
+   (and (featurep 'projectile) (projectile-project-root))
+   ;; Default to the current directory if no method works
+    default-directory))
+
+(defvar epry-sessions (make-hash-table :test 'equal)
+  "Hash table storing epry-ui instances keyed by their project root.")
+
+(define-derived-mode epry-mode fundamental-mode "Epry"
+  "Major mode for the epry debugger interface."
+  (defvar-local epry-current-ui nil "Local instance of the epry-ui class.")
+  (setq buffer-read-only t)
+  (setq-local buffer-save-without-query t)
+  (display-line-numbers-mode -1))
+
+(defun epry-start (&optional command)
+  "Start EPry by initializing the UI and displaying it, optionally running a COMMAND."
+  (interactive)
+  (let* ((root (epry-project-root))
+         (ui (or (gethash root epry-sessions)  ; Check for an existing session first
+                 (let ((new-ui (epry-ui :project-root root)))  ; Create and initialize new UI
+                   (epry-ui-init new-ui)
+                   (puthash root new-ui epry-sessions)
+                   new-ui))))
+    (with-current-buffer (oref ui buffer)
+      (setq-local epry-current-ui ui)
+      (when command  ; If a command is provided, run it.
+        (epry-run-command ui command)))
+    (switch-to-buffer-other-window (oref ui buffer))
+    ui)) ; Ensure that the UI instance is returned
+
+(defun epry-execute-command (&optional command)
+  "Prompt for a command, or use the provided COMMAND, and execute it in the context of the current or new EPry session."
+  (interactive)
+  ;; If no command is provided, prompt the user for one
+  (unless command
+    (setq command (read-from-minibuffer "Command: ")))
+
+  (let ((ui (if (and (derived-mode-p 'epry-mode) (bound-and-true-p epry-current-ui))
+                epry-current-ui  ; Use the existing UI instance if available
+              (epry-start))))  ; Otherwise, start a new session
+    ;; Only run the command if ui is correctly set and we're in the right mode
+    (when (and ui (derived-mode-p 'epry-mode))
+      (epry-run-command ui command))))
+
+(defun epry-bundle-install ()
+  "Run 'bundle install' in the context of the current or new EPry session."
+  (interactive)
+  (epry-execute-command "bundle install"))
+
+(defun epry-test ()
+  "Run 'bundle exec rspec' in the context of the current or new EPry session."
+  (interactive)
+  (epry-execute-command "bundle exec rspec"))
+
+(defun epry-rails ()
+  "Run 'bundle exec rails s' in the context of the current or new EPry session."
+  (interactive)
+  (epry-execute-command "bundle exec rails s"))
+
+(defun epry-pprint-sessions ()
+  "Pretty-print all entries in the `epry-sessions` hash table to a dedicated buffer."
+  (interactive)
+  (let ((output-buffer (get-buffer-create "*EPry-Sessions*")))
+    (with-current-buffer output-buffer
+      (read-only-mode -1)
+      (erase-buffer)
+      (maphash (lambda (key value)
+                 (pp `(,key ,value) output-buffer))
+               epry-sessions)
+      (read-only-mode 1))
+    (pop-to-buffer output-buffer)))
+
+(defclass epry-ui ()
+  ((buffer :initarg :buffer
+           :initform "*EPry*"
+           :type t
+           :documentation "The buffer associated with the EPry session.")
+   (window :initarg :window
+           :type t
+           :documentation "The window displaying the EPry buffer.")
+   (project-root :initarg :project-root
+                 :initform nil
+                 :type (or null string)
+                 :documentation "The root directory of the current project."))
+  "Class representing the UI for EPry.")
+
+(cl-defmethod epry-ui-init ((ui epry-ui))
+  "Initialize the EPry UI: create the buffer based on the project root and set up the window."
+  (with-slots (buffer window project-root) ui
+    ;; Determine the project root and store it in the instance
+    (setf project-root (expand-file-name (epry-project-root)))
+    ;; Set the buffer name to include the project directory name
+    (setq buffer (get-buffer-create (format "*EPry* - %s" (file-name-nondirectory (directory-file-name project-root)))))
+    (with-current-buffer buffer
+      (setq-local epry-current-ui ui)
+      (epry-mode))
+    (setq window (display-buffer buffer))))
+
+(cl-defmethod epry-create-command ((ui epry-ui) command)
+  "Create a shell command string adjusted for the stored project directory."
+  (with-slots (project-root) ui
+    (let* ((expanded-root (expand-file-name project-root))  ; Ensure path is expanded.
+            (full-command (concat "cd " (shell-quote-argument expanded-root) " && "
+                            epry-prefix-command
+                            "ruby --version" " && "
+                            epry-prefix-command
+                            command)))
+      ;(message "Executing command: %s" full-command)
+     full-command)))
+
+(cl-defmethod epry-run-command ((ui epry-ui) command)
+  "Execute a COMMAND using the shell specified in `epry-shell-path` with output in the appropriate buffer."
+  (with-slots (buffer) ui
+    (let ((output-buffer (get-buffer-create buffer))
+          (process-command (list epry-shell-path "-l" "-c" (epry-create-command ui command))))
+      (with-current-buffer output-buffer
+        ;; Ensure buffer is writable
+        (let ((inhibit-read-only t))
+          (erase-buffer))  ; Clear previous contents
+        ;; Start the process
+        (let ((process (make-process
+                        :name "epry-command-process"
+                        :buffer output-buffer
+                        :command process-command
+                        :filter (lambda (proc string)
+                                  (with-current-buffer (process-buffer proc)
+                                    (let ((inhibit-read-only t))
+                                      (goto-char (point-max))
+                                      (insert (ansi-color-apply string))))))))
+          (set-process-sentinel process
+                                (lambda (proc event)
+                                  (when (string-match-p "\\`finished" event)
+                                    (with-current-buffer (process-buffer proc)
+                                      (setq buffer-read-only t))))))))))
+
+
 
 (provide 'epry)
 ;;; epry.el ends here
