7734480c4147cfcc53e9940a3d1c516584ef8698
Quint Pieters <quintgorssel@gmail.com>
2024-05-31 15:30:44 +0200

    chore(wip): Initial improvement of liniting

--- epry.el
+++ epry.el
@@ -44,7 +44,7 @@
   :group 'epry)
 
 (defcustom epry-debug-statement "require 'debug/open'; DEBUGGER__.open #fixme"
-  "Ruby debug statement to insert"
+  "Ruby debug statement to insert."
   :type 'string
   :group 'epry)
 
@@ -61,12 +61,12 @@
 (defun epry-project-root ()
   "Return the root directory of the project."
   (expand-file-name (or
-   ;; Try to find the Git root via VC first
-   (vc-root-dir)
-   ;; Fallback to projectile if vc-root-dir is not available or fails
-   (and (featurep 'projectile) (projectile-project-root))
-   ;; Default to the current directory if no method works
-    default-directory)))
+                     ;; Try to find the Git root via VC first.
+                     (vc-root-dir)
+                     ;; Fallback to projectile if vc-root-dir is not available or fails.
+                     (and (featurep 'projectile) (projectile-project-root))
+                     ;; Default to the current directory if no method works.
+                     default-directory)))
 
 (defvar epry-sessions (make-hash-table :test 'equal)
   "Hash table storing epry-ui instances keyed by their project root.")
@@ -102,8 +102,8 @@
   "Start EPry by initializing the UI and displaying it, optionally running a COMMAND."
   (interactive)
   (let* ((root (epry-project-root))
-         (ui (or (gethash root epry-sessions)  ; Check for an existing session first
-                 (let ((new-ui (epry-ui :project-root root)))  ; Create and initialize new UI
+         (ui (or (gethash root epry-sessions)  ; Check for an existing session first.
+                 (let ((new-ui (epry-ui :project-root root)))  ; Create and initialize new UI.
                    (epry-ui-init new-ui)
                    (puthash root new-ui epry-sessions)
                    new-ui))))
@@ -112,7 +112,7 @@
       (when command  ; If a command is provided, run it.
         (epry-run-command ui command)))
     (switch-to-buffer-other-window (oref ui buffer))
-    ui)) ; Ensure that the UI instance is returned
+    ui))  ; Ensure that the UI instance is returned.
 
 (defun epry-cleanup-session (project-root)
   "Remove the session associated with PROJECT-ROOT from the epry-sessions hash table."
@@ -135,11 +135,13 @@
 (defun epry-get-or-create-ui ()
   "Get or create an `epry-ui` instance depending on the current context."
   (if (and (derived-mode-p 'epry-mode) (bound-and-true-p epry-current-ui))
-      epry-current-ui  ; Return the existing UI if already present and correct mode
-    (epry-start)))     ; Start a new UI session if not present
+      epry-current-ui  ; Return the existing UI if already present and correct mode.
+    (epry-start)))  ; Start a new UI session if not present.
 
 (defun epry-execute-command (&optional command callback)
-  "Prompt for a command, or use the provided COMMAND, and execute it in the context of the current or new EPry session. Optionally, run CALLBACK after the command finishes."
+  "Prompt for a command, or use the provided COMMAND,
+and execute it in the context of the current or new EPry session.
+Optionally, run CALLBACK after the command finishes."
   (interactive)
   (let ((ui (epry-get-or-create-ui)))
     (unless command
@@ -191,7 +193,7 @@
    (project-root :initarg :project-root
                  :initform nil
                  :type (or null string)
-     :documentation "The root directory of the current project.")
+                 :documentation "The root directory of the current project.")
    (last-command :initarg :last-command
                  :initform nil
                  :type (or null string)
@@ -201,9 +203,9 @@
 (cl-defmethod epry-ui-init ((ui epry-ui))
   "Initialize the EPry UI: create the buffer based on the project root and set up the window."
   (with-slots (buffer window project-root) ui
-    ;; Determine the project root and store it in the instance
+    ;; Determine the project root and store it in the instance.
     (setf project-root (expand-file-name (epry-project-root)))
-    ;; Set the buffer name to include the project directory name
+    ;; Set the buffer name to include the project directory name.
     (setq buffer (get-buffer-create (format "*EPry* - %s" (file-name-nondirectory (directory-file-name project-root)))))
     (with-current-buffer buffer
       (setq-local epry-current-ui ui)
@@ -217,10 +219,10 @@
   "Create a shell command string adjusted for the stored project directory."
   (with-slots (project-root) ui
     (let* ((expanded-root (expand-file-name project-root))  ; Ensure path is expanded.
-           (epry-gemfile (expand-file-name epry-gemfile-name project-root))  ; Path to the epry Gemfile
+           (epry-gemfile (expand-file-name epry-gemfile-name project-root))  ; Path to the epry Gemfile.
            (bundle-gemfile-setting (if (file-exists-p epry-gemfile)
                                        (concat "BUNDLE_GEMFILE=" (shell-quote-argument epry-gemfile) " ")
-                                     ""))  ; Set BUNDLE_GEMFILE if the file exists
+                                     ""))  ; Set BUNDLE_GEMFILE if the file exists.
            (full-command (concat "cd " (shell-quote-argument expanded-root) " && "
                                  epry-prefix-command
                                  "ruby --version" " && "
@@ -231,15 +233,16 @@
       full-command)))
 
 (cl-defmethod epry-run-command ((ui epry-ui) command &optional callback)
-  "Execute a COMMAND using the shell specified in `epry-shell-path` with output in the appropriate buffer. Optionally, run CALLBACK after the command finishes."
+  "Execute a COMMAND using the shell specified in `epry-shell-path` with output in the appropriate buffer.
+Optionally, run CALLBACK after the command finishes."
   (with-slots (buffer) ui
     (let ((output-buffer (get-buffer-create buffer))
           (process-command (list epry-shell-path "-l" "-c" (epry-create-command ui command))))
       (with-current-buffer output-buffer
-        ;; Ensure buffer is writable
+        ;; Ensure buffer is writable.
         (let ((inhibit-read-only t))
-          (erase-buffer))  ; Clear previous contents
-        ;; Start the process
+          (erase-buffer))  ; Clear previous contents.
+        ;; Start the process.
         (make-process
          :name "epry-command-process"
          :buffer output-buffer
@@ -250,7 +253,7 @@
                        (goto-char (point-max))
                        (insert (ansi-color-apply string))
                        (epry-auto-scroll)
-                       ;; Check for the specific line
+                       ;; Check for the specific line.
                        (when (string-match "DEBUGGER: wait for debugger connection..." string)
                          (epry-debugger-attach ui)))))
          :sentinel (lambda (proc event)
@@ -269,18 +272,18 @@
   (with-slots (project-root) ui
     (let ((original-gemfile (expand-file-name (epry-get-project-gemfile-name ui) project-root))
           (epry-gemfile (expand-file-name epry-gemfile-name project-root)))
-      ;; Check if the original Gemfile exists
+      ;; Check if the original Gemfile exists.
       (if (not (file-exists-p original-gemfile))
           (message "No Gemfile found in the project root: %s" project-root)
         (progn
-          ;; Create the new epry-gemfile with eval_gemfile
+          ;; Create the new epry-gemfile with eval_gemfile.
           (with-temp-file epry-gemfile
             (insert (format "# EPry generated Gemfile\n\n"))
             (insert (format "eval_gemfile '%s'\n\n" (file-name-nondirectory original-gemfile)))
-            ;; Append the gem "debug" line directly
+            ;; Append the gem "debug" line directly.
             (insert "gem 'debug', '>= 1.0.0'\n"))
           (message "Created EPRy Gemfile at: %s" epry-gemfile)
-          ;; Copy the original Gemfile.lock if it exists
+          ;; Copy the original Gemfile.lock if it exists.
           (let ((original-gemfile-lock (expand-file-name (concat (epry-get-project-gemfile-name ui) ".lock") project-root))
                 (epry-gemfile-lock (expand-file-name (concat epry-gemfile-name ".lock") project-root)))
             (when (file-exists-p original-gemfile-lock)
@@ -302,9 +305,9 @@
             (with-temp-buffer
               (insert-file-contents epry-gemfile-lock)
               (goto-char (point-min))
-              ;; Print the contents of the buffer for debugging
+              ;; Print the contents of the buffer for debugging.
               (message "Contents of epry-gemfile-lock:\n%s" (buffer-string))
-              (let ((case-fold-search nil))  ; Make the search case-sensitive
+              (let ((case-fold-search nil))  ; Make the search case-sensitive.
                 (while (search-forward original-url nil t)
                   (message "Found a match for original-url at position %d" (point))
                   (replace-match new-url)))
@@ -340,7 +343,7 @@
       (epry-create-project-gemfile ui))))
 
 (defun epry-insert-ruby-debug-statement ()
-  "Inserts the Ruby debug statement defined by `epry-debug-statement` at the current cursor position."
+  "Insert the Ruby debug statement defined by `epry-debug-statement` at the current cursor position."
   (interactive)
   (insert epry-debug-statement))
 
